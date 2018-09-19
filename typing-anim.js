11e676c91e61d94caaf82b0437705b11d11e8759
Philemon <philebaba47@outlook.com>
2018-09-19 12:20:11 +0100

    removal

--- typing-anim.js
+++ typing-anim.js
@@ -12,47 +12,4 @@ $(document).ready(function() {
       typeSpeed: 100,
       loop: true,
     });
-  });
-
-  // ---------- smooth scroll ---------- //
-
-  // Select all links with hashes
-  $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 700);
-      }
-    }
-  });
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {
-    scrollFunction()
-  };
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      document.getElementById("scrollTop").style.display = "block";
-    } else {
-      document.getElementById("scrollTop").style.display = "none";
-    }
-  }
-
-});
+  })});
